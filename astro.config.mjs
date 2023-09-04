3a001427097ce61ebb41f565508ff387f10b61e8
ANUSREEPURUSHOTHAMANstartupmission <121005558+ANUSREEPURUSHOTHAMANstartupmission@users.noreply.github.com>
2023-09-04 09:41:07 +0530

    changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ANUSREEPURUSHOTHAMANstartupmission.github.io',
+  base: '/womenstartupsummit_2023',
   integrations: [tailwind(), svelte()]
 });
